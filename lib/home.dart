5e121ad69ba9a0ec6785a895c81d8d14777e72c6
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-21 12:46:57 +0900

    draw circle

--- lib/home.dart
+++ lib/home.dart
@@ -9,3 +9,44 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+class _CustomPainter extends CustomPainter {
+  _CustomPainter(this._pathOperation);
+
+  final PathOperation _pathOperation;
+
+  final _paint = Paint()
+    ..color = Colors.orange
+    ..style = PaintingStyle.fill;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (size.isEmpty) {
+      return;
+    }
+
+    final centerX = size.width / 2;
+    final centerY = size.height / 2;
+
+    final path = Path.combine(
+      _pathOperation,
+      Path()
+        ..addOval(Rect.fromCircle(
+          center: Offset(centerX - 50, centerY),
+          radius: 100,
+        ))
+        ..close(),
+      Path()
+        ..addOval(Rect.fromCircle(
+          center: Offset(centerX + 50, centerY),
+          radius: 100,
+        ))
+        ..close(),
+    );
+
+    canvas.drawPath(path, _paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
